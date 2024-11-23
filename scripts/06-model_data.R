e400362f91d13c2f7cb1d4ec2dd5a644b40b667b
Krishna Kumar <krishna.kumar@mail.utoronto.ca>
2024-11-23 17:08:35 -0500

    finished model creation and modelsummary. yet to do diagnostics. also added another graph of the affordability index in the data section. also finished results section

--- scripts/06-model_data.R
+++ scripts/06-model_data.R
@@ -10,10 +10,42 @@
 
 #### Workspace setup ####
 library(tidyverse)
-library(rstanarm)
+library(arrow)
+library(here)
 
 #### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+analysis_data <- read_parquet(here("data", "02-analysis_data", "analysis_data.parquet"))
+
+## Model 1: Only using CPI as a predictor ##
+
+model_1 <- lm(`Affordability Index` ~ `CPI (Index)`, data = analysis_data)
+
+## Save Model 1 ##
+
+saveRDS(model_1, "models/model_1_CPI.rds")
+
+
+## Model 2: Using CPI and Housing Suply as Predictors ##
+
+model_2 <- lm(`Affordability Index` ~ `CPI (Index)` + `House Supply (Month's Supply)`, data = analysis_data)
+
+## Saving Model 2 ## 
+
+saveRDS(model_2, "models/model_2_CPI_HousingSupply.rds")
+
+## Model 3: Using CPI, Housing Supply, and Interest Rate as predictors ##
+
+model_3 <- lm(`Affordability Index` ~ `CPI (Index)` + `House Supply (Month's Supply)` + `Interest Rate (%)`, data = analysis_data)
+
+## Saving Model 3 ##
+
+saveRDS(model_3, "models/model_3_Full.rds")
+
+
+
+
+
+
 
 ### Model data ####
 first_model <-
